3021a9ce1e00270fa4db2b44132072af75fa15e5
tumelorabale <133318673+tumelorabale@users.noreply.github.com>
2023-07-16 21:32:03 -0500

    widgets update2

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,7 +1,7 @@
 import Proj1 from "../assets/proj1.jpg";
 import Proj2 from "../assets/proj2.jpg";
-import Proj3 from "../assets/proj3.webp";
-import Proj4 from "../assets/proj4.webp";
+import Proj3 from "../assets/proj3.jpg";
+import Proj4 from "../assets/proj4.jpg";
 import Proj5 from "../assets/proj5.jpg";
 import Proj6 from "../assets/proj6.png";
 
